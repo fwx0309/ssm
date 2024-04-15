337a3e90e9605672b27260947b963fc37f4f4a81
fwx <345025106@qq.com>
2024-04-16 01:34:08 +0800

    d16_spring_mvc_extension 过滤器和拦截器

---
+++ d16_spring_mvc_extension/src/main/java/org/fwx/controller/TestController.java
@@ -0,0 +1,18 @@
+package org.fwx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description:
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test/hello")
+    public String testHello(){
+//        System.out.println(1/0);
+        return "success";
+    }
+
+}
